535d343c2b0f9e288783703b60eb218e635899ce
annstar-dl <ann.starynska@gmail.com>
2025-08-28 17:24:27 -0400

    Add PaddleSeg directory path and update data path for image conversion

--- seg_subtract.sh
+++ seg_subtract.sh
@@ -8,7 +8,7 @@ SEGMENTATION_DIR="/home/astar/Projects/membrane_detection/out/unet_membrane_256x
 MEMBRANE_SUBTRACTION_DIR="/home/astar/Projects/VesicleProjection" # Path to the VesicleProjection project directory
 INPUT_FILE_FORMAT="mrc"  # Change to "tif" if input files are in TIFF format
 SEGMENTATION_MODEL_FORMAT="paddleseg" # "onnx" or "paddleseg"
-
+PADDLESEG_DIR="/home/astar/Projects/PaddleSeg" # Path to the PaddleSeg project directory
 # Check if the directory argument is provided
 if [ $# -lt 3 ]; then
     echo "Usage: $0 <output_directory_path> <input_files_directory> <downsample_factor_segmentation>"
@@ -21,7 +21,7 @@ mkdir -p $1
 #go into the new directory
 cd $1
 # copy input files directory to an output directory
-cp -r $2 .
+#cp -r $2 .
 echo "Copied MRC files from $2 to $PWD/$MRC_DIR"
 # create a jpg directory to store the converted images
 mkdir -p "images_jpg"
@@ -39,11 +39,11 @@ if [ ${SEGMENTATION_MODEL_FORMAT} == "onnx" ]; then
     conda run -n paddleseg_onnx python "$MEMBRANE_DETECTION_DIR/export_onnx/run_onnx.py" \
     --model_dir "${SEGMENTATION_DIR}/result" \
     --onnx_fname model.onnx \
-    --data_path "${PWD}/images_jpg" \
+    --data_path "${PWD}/images_jpg/${MRC_DIR}_jpeg_ds${$3}" \
     --save_dir "${PWD}"
 elif [ ${SEGMENTATION_MODEL_FORMAT} == "paddleseg" ]; then
     echo "Using PaddleSeg segmentation model format."
-    PADDLESEG_DIR ="/home/astar/Projects/PaddleSeg" # Path to the PaddleSeg project directory
+
     # Perform segmentation using PaddleSeg
     conda run -n paddleseg python "${PADDLESEG_DIR}/deploy/python/infer.py" \
     --config "${SEGMENTATION_DIR}/result/deploy.yaml" \
