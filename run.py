ed4b62f1e550a67182a45475982386bb36867b6c
annstar-dl <ann.starynska@gmail.com>
2025-09-29 14:06:41 -0400

    from run.py remove mrc input file option

--- run.py
+++ run.py
@@ -18,10 +18,6 @@ def read_img(fpath,in_format,downsample_factor=1, mask=False):
     print(f"Processing image: {fpath}")
     if not os.path.exists(fpath):
         raise FileNotFoundError(f"File {fpath} does not exist.")
-    if "mrc" in in_format:
-        img, header = load_mrc(fpath,downsample_factor=downsample_factor)
-        img = img.astype(np.float64)
-        return img, header
     else:
         img = Image.open(fpath)
         img = np.array(img,dtype = np.float64)
@@ -31,7 +27,7 @@ def read_img(fpath,in_format,downsample_factor=1, mask=False):
             img = (img >0.5).astype(np.float64)
         if downsample_factor > 1:
             img = transform.rescale(img, 1/downsample_factor, anti_aliasing=True, preserve_range=True)
-        return img, None
+    return img
 
 
 def save_im(img, fpath):
@@ -45,17 +41,6 @@ def save_im(img, fpath):
     img = Image.fromarray(img,"L")
     img.save(fpath)
 
-def save_im_mrc(img, fpath, header):
-    """Save the image to a MRC file.
-    Args:
-        img (numpy.ndarray): Image array to save.
-        fpath (str): Path to save the image file.
-    """
-    img = img.detach().cpu().numpy()
-    with mrcfile.new(fpath, overwrite=True) as mrc:
-        mrc.set_data(img.astype(np.float32))
-
-
 def main(args):
     imgs_path = os.path.join(args.dataset_path, args.imgs_dir)
     masks_path = os.path.join(args.dataset_path, args.masks_dir)
@@ -65,19 +50,20 @@ def main(args):
 
         basename = os.path.splitext(fpath)[0]
         if os.path.exists(os.path.join(args.imgsout_path + "_jpeg", basename + ".jpeg")) and \
-              os.path.exists(os.path.join(args.imgsout_path + "_mat", basename + ".mat")) and \
-                os.path.exists(os.path.join(args.imgsout_path + "_mrc", basename + ".mrc")):
+              os.path.exists(os.path.join(args.imgsout_path + "_mat", basename + ".mat")):
             print(f"Output for {basename} already exists. Skipping...")
             continue
 
         img_fname = basename+"."+args.in_format
-        img, header = read_img(os.path.join(imgs_path, img_fname),args.in_format, args.downsample_factor)
+        img = read_img(os.path.join(imgs_path, img_fname),args.in_format, args.downsample_factor)
         mask = read_img(os.path.join(masks_path, basename + ".png"),"png",args.downsample_factor,True)[0]
         # check if the image is the same size as the mask
         if img.shape[:2] != mask.shape[:2]:
             raise ValueError(
                 f"Image {img_fname} and mask {basename}.png must have the same dimensions. Image shape: {img.shape}, Mask shape: {mask.shape}")
+        # run membrane subtraction algorithm
         imgout, sub_img = membrane_subtract(img, mask)
+
         # add background back to the subtracted image
         if "mat" in args.out_format:
             # Save as .mat file if specified
@@ -89,9 +75,6 @@ def main(args):
         if "png" in args.out_format:
             # Save as .png file if specified
             save_im(sub_img, os.path.join(args.imgsout_path+"_png", basename + ".png"))
-        if "mrc" in args.out_format:
-            # Save as .png file if specified
-            save_im_mrc(sub_img, os.path.join(args.imgsout_path+"_mrc", basename + ".mrc"), header)
         if args.save_reconstruction:
             save_im(imgout, os.path.join(args.imgsout_reconstructed_path, basename + ".tif"))
             np.save(os.path.join(args.imgsout_reconstructed_path, basename + ".npy"), imgout)
@@ -102,7 +85,7 @@ if __name__=="__main__":
     parser.add_argument("-id","--imgs_dir", type=str, default="images", help="Directory containing images.")
     parser.add_argument("-md","--masks_dir", type=str, default="labels", help="Directory containing labels for images.")
     parser.add_argument("-s","--sigma", type=float, default=24.0, help="Sigma for Gaussian filter to flatten background.")
-    parser.add_argument("--in_format", type=str,choices=["mrc","jpeg","jpg","tif","tiff"],help="Format of input images. Default is mrc", default="mrc")
+    parser.add_argument("--in_format", type=str,choices=["jpeg","jpg","tif","tiff"],help="Format of input images. Default is mrc", default="mrc")
     parser.add_argument("--out_format", nargs="+", default="png", help="List of file format to save subracted images. Choices are .mat,.png.,mrc formats")
     parser.add_argument("-ds","--downsample_factor", type=int, default=1, help="Factor by which to downsample the images (default: 1).")
     parser.add_argument("--save_reconstruction", action="store_true", default=False, help="Save the reconstructed membranes.")
