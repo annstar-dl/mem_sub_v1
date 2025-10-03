b0ae7b77e5a3ca469329495a765411c2a2f77cd0
annstar-dl <ann.starynska@gmail.com>
2025-10-03 14:18:01 -0400

    Update segmentation scripts to remove downsample factor requirement

--- seg_subtract.sh
+++ seg_subtract.sh
@@ -10,8 +10,8 @@ INPUT_FILE_FORMAT="mrc"  # Change to "tif" if input files are in TIFF format
 SEGMENTATION_MODEL_FORMAT="paddleseg" # "onnx" or "paddleseg"
 PADDLESEG_DIR="/home/astar/Projects/PaddleSeg" # Path to the PaddleSeg project directory
 # Check if the directory argument is provided
-if [ $# -lt 3 ]; then
-    echo "Usage: $0 <output_directory_path> <input_files_directory> <downsample_factor_segmentation>"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <output_directory_path> <input_files_directory>"
     exit 1
 fi
 
@@ -21,7 +21,7 @@ mkdir -p $1
 #go into the new directory
 cd $1
 # copy input files directory to an output directory
-#cp -r $2 .
+cp -r $2 .
 echo "Copied MRC files from $2 to $PWD/$MRC_DIR"
 # create a jpg directory to store the converted images
 mkdir -p "images_jpg"
@@ -31,7 +31,7 @@ mkdir -p "labels"
 # Convert mrc files to jpg for segmentation
 echo "Converting MRC files to JPG..."
 echo "Using Membrane Subtraction directory: $PWD/${MRC_DIR}"
-conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/mrc2jpg.py" "$PWD/${MRC_DIR}" -o "$PWD/images_jpg" --format "jpeg" -ds $3
+conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/mrc2jpg.py" "$PWD/${MRC_DIR}" -o "$PWD/images_jpg" --format "jpeg" -dsa --scale
 echo "Converted MRC files to JPG in $PWD/images_jpg"
 
 if [ ${SEGMENTATION_MODEL_FORMAT} == "onnx" ]; then
@@ -39,7 +39,7 @@ if [ ${SEGMENTATION_MODEL_FORMAT} == "onnx" ]; then
     conda run -n paddleseg_onnx python "$MEMBRANE_DETECTION_DIR/export_onnx/run_onnx.py" \
     --model_dir "${SEGMENTATION_DIR}/result" \
     --onnx_fname model.onnx \
-    --data_path "${PWD}/images_jpg/${MRC_DIR}_jpeg_ds${$3}" \
+    --data_path "${PWD}/images_jpg/${MRC_DIR}_ds" \
     --save_dir "${PWD}"
 elif [ ${SEGMENTATION_MODEL_FORMAT} == "paddleseg" ]; then
     echo "Using PaddleSeg segmentation model format."
@@ -47,7 +47,7 @@ elif [ ${SEGMENTATION_MODEL_FORMAT} == "paddleseg" ]; then
     # Perform segmentation using PaddleSeg
     conda run -n paddleseg python "${PADDLESEG_DIR}/deploy/python/infer.py" \
     --config "${SEGMENTATION_DIR}/result/deploy.yaml" \
-    --image_path "$PWD/images_jpg" \
+    --image_path "$PWD/images_jpg/${MRC_DIR}_ds" \
     --save_dir "$PWD/labels_pseudcolor" \
     # convert the pseudo-color masks to binary masks
     conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/color2binary.py" "$PWD/labels_pseudcolor"
@@ -59,10 +59,10 @@ fi
 echo "Converted pseudo-color masks to binary masks in $PWD/labels"
 
 # Subtract the predicted masks from the original micrographs
-conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/run.py" \
+conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/run_mrc.py" \
  -dp ${PWD} -id ${MRC_DIR} -md "labels" \
  --in_format ${INPUT_FILE_FORMAT} \
- --out_format ["mrc","mat","png"] -ds $3
+ --out_format "mrc" "png"
 echo "Subtracted masks from original micrographs and saved results in $PWD/reconstructions"
 
 
--- subtract.sh
+++ subtract.sh
@@ -10,19 +10,16 @@ INPUT_FILE_FORMAT="jpg"  # Change to "tif" if input files are in TIFF format
 SEGMENTATION_MODEL_FORMAT="paddleseg" # "onnx" or "paddleseg"
 PADDLESEG_DIR="/home/astar/Projects/PaddleSeg" # Path to the PaddleSeg project directory
 # Check if the directory argument is provided
-if [ $# -lt 3 ]; then
-    echo "Usage: $0 <output_directory_path> <input_files_directory> <downsample_factor_segmentation>"
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <output_directory_path> <input_files_directory>"
     exit 1
 fi
 
 MRC_DIR=$(basename "$2")
 cd $1
 # Subtract the predicted masks from the original micrographs
-conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/run.py" \
- -dp ${PWD} -id ${MRC_DIR} -md "labels" \
+conda run -n ves_seg python "${MEMBRANE_SUBTRACTION_DIR}/run_mrc.py" \
+ -dp ${PWD} -id ${MRC_DIR} -md "labels_dilated" \
  --in_format ${INPUT_FILE_FORMAT} \
- --out_format "png" -ds $3 \
- --save_reconstruction
+ --out_format "mrc" "png"
 echo "Subtracted masks from original micrographs and saved results in $PWD/reconstructions"
-
-
