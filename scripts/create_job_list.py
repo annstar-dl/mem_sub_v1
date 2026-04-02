977140b115e85059ca659b38438328207d27abf6
astar <anna.starynska@yale.edu>
2026-04-02 17:20:32 -0400

    fix: remove unnecessary delete_mrc_ext call in create_job_list.py

--- scripts/create_job_list.py
+++ scripts/create_job_list.py
@@ -97,7 +97,7 @@ def create_job_list(data_dir_path, job_file_path,save_dir_path,
             # process the whole batch (e.g., submit jobs in groups)
             f.write(prefix)
             for filename in batch:
-                filename= delete_mrc_ext(strip_leading_dot_slash(filename))
+                filename= strip_leading_dot_slash(filename)
                 f.write(f"bash /nfs/roberts/scratch/pi_hdt2/as4873/mem_sub_v1/scripts/seg_subtract_v1.sh {filename};")
             f.write("\n")  # Separate batches with a newline
             nb_of_jobs_iter += 1
