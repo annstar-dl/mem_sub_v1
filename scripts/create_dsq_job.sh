b0988803d4afe13642c77370231bfea402e5e426
astar <anna.starynska@yale.edu>
2026-04-02 15:59:24 -0400

    fix: update create_dsq_job.sh to remove SEG_MODEL_PATH parameter and change partition to gpu_rtx6000

--- scripts/create_dsq_job.sh
+++ scripts/create_dsq_job.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #create a job_list for dsq submission
 #Note: slurm jobs start in the directory from which your job was submitted.
-if [ "$#" -ne 7 ]; then
-    echo "Usage: $0 DATASET_PATH SEG_MODEL_PATH SAVE_DIR_PATH JOB_ARRAY_NAME SAVE_ANGLE SAVE_SUB SHOW_OUTPUT"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: $0 DATASET_PATH SAVE_DIR_PATH JOB_ARRAY_NAME SAVE_ANGLE SAVE_SUB SHOW_OUTPUT"
     exit 1
 fi
 DATASET_PATH=$1
-SEG_MODEL_PATH=$2
 SAVE_DIR_PATH=$3
 JOB_ARRAY_NAME=$4
 SAVE_ANGLE=$5
@@ -17,12 +16,12 @@ module load miniconda
 module load dSQ
 conda activate ves_seg
 python scripts/create_job_list.py -ddp ${DATASET_PATH} -jfp "./joblist.txt" \
-    -segmp ${SEG_MODEL_PATH} -savedp ${SAVE_DIR_PATH} \
-    --save_angle_flag=${SAVE_ANGLE} --save_sub_flag=${SAVE_SUB} \
+    -savedp ${SAVE_DIR_PATH} --save_angle_flag=${SAVE_ANGLE} \
+    --save_sub_flag=${SAVE_SUB} \
 # the joblist.txt will be created in the current directory
 # Now create the dsq job submission script
 if [ "$SHOW_OUTPUT" -eq 1 ]; then
-    dsq --job-file joblist.txt --mem=5G --cpus-per-task=4 --gres=gpu:1 -t 20:00 --partition=scavenge_gpu --mail-type ALL  --batch-file="${JOB_ARRAY_NAME}_${TIMESTEMP}_dsq_job.sh"
+    dsq --job-file joblist.txt --mem=5G --cpus-per-task=4 --gres=gpu:1 -t 20:00 --partition=gpu_rtx6000 --mail-type ALL  --batch-file="${JOB_ARRAY_NAME}_${TIMESTEMP}_dsq_job.sh"
 else
-    dsq --job-file joblist.txt --mem=5G --cpus-per-task=4 --gres=gpu:1 -t 20:00 --partition=scavenge_gpu --mail-type ALL  --batch-file="${JOB_ARRAY_NAME}_${TIMESTEMP}_dsq_job.sh" --output=/dev/null
+    dsq --job-file joblist.txt --mem=5G --cpus-per-task=4 --gres=gpu:1 -t 20:00 --partition=gpu_rtx6000 --mail-type ALL  --batch-file="${JOB_ARRAY_NAME}_${TIMESTEMP}_dsq_job.sh" --output=/dev/null
 fi
